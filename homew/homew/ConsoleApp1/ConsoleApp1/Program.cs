efa3e7646e11e40d4b4525e89db8da39f6e32137
Eli <aliasa@code.edu.az>
2022-05-26 13:02:23 +0400

    hometask

---
+++ homew/homew/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int nameList(string[] names)
+        {
+            int count = 0;
+            for (int i = 0; i < names.Length; i++)
+            {
+
+                if (names[i].Length > 5)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    } // Verilmis adlar siyahisinda (string array) nece adin uzunlugunun 5-den boyuk oldugunu tapan proqram
+    
+
+}
+
+
